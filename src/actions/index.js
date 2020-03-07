8108e608d8157943ae50407539a4b4c1a28ca6d4
Nikki <nikkiweat@gmail.com>
2020-03-08 00:32:02 +0800

    Define REMOVE_ITEM and SUBTRACT_QUANTITY actions, ADD_TO_CART action will be used when increasing the quantity since the necessary logic is in place to update the item quantity and totals if it already exists in the cart

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
-import { ADD_TO_CART } from "./types"
+import { ADD_TO_CART, REMOVE_ITEM, SUBTRACT_QUANTITY } from "./types"
 
 export const addToCart = id => {
   return { type: ADD_TO_CART, payload: id }
+}
+
+export const removeItem = id => {
+  return { type: REMOVE_ITEM, payload: id }
+}
+
+export const subtractQuantity = id => {
+  return { type: SUBTRACT_QUANTITY, payload: id }
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
 export const ADD_TO_CART = "ADD_TO_CART"
+export const REMOVE_ITEM = "REMOVE_ITEM"
+export const SUBTRACT_QUANTITY = "SUBTRACT_QUANTITY"
